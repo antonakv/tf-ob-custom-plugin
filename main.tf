671a83e6bafe5fc3f95cb29128c6b43dac69eee9
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2021-06-02 14:29:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,7 @@ terraform {
   }
 }
 
-resource "null_resource" "mynull" {
-  count = 3
-
-  provisioner "local-exec" {
-    command = "echo ${count.index}"
-  }
-
-}
-
-data "extip" "external_ip" {
-}
+data "extip" "external_ip" {}
 
 output "external_ip" {
   value = data.extip.external_ip.ipaddress
